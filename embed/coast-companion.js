01719baac7b04d310303923c0695eafa8de9fd0a
Devam Sisodraker <devam@student.ubc.ca>
2024-02-20 21:53:48 -0800

    added embed

---
+++ embed/coast-companion.js
@@ -0,0 +1,46 @@
+(function (embedUrl, defaultHidden, buttonColor) {
+
+    // Insert iFrame into the Page
+    var iframe = document.createElement('iframe');
+    iframe.src = embedUrl;
+    iframe.frameBorder = '0';
+    iframe.style.height = '400px';
+    iframe.style.width = '400px';
+    iframe.style.borderRadius = '2em';
+    iframe.style.position = 'fixed';
+    iframe.style.bottom = '10px';
+    iframe.style.right = '70px';
+    iframe.style.display = 'none';
+    iframe.style.zIndex = '100';
+    document.body.appendChild(iframe);
+    
+    // Insert the Show/Hide button
+    var toggleBtn = document.createElement('button');
+    var hidden = defaultHidden;
+    iframe.style.display = hidden ? 'none' : 'block';
+    toggleBtn.innerHTML = '<b>C</b>C';
+    toggleBtn.onclick = function () {
+        hidden = !hidden;
+        iframe.style.display = hidden ? 'none' : 'block';
+        toggleBtn.style.borderColor = buttonColor;
+        toggleBtn.style.borderLeftColor = buttonColor;
+        toggleBtn.style.borderRightColor = buttonColor;
+        toggleBtn.style.accentColor = buttonColor;
+        toggleBtn.style.backgroundColor = buttonColor;
+        toggleBtn.style.backgroundColor = buttonColor;
+    };
+    toggleBtn.style.position = 'fixed';
+    toggleBtn.style.bottom = '10px';
+    toggleBtn.style.right = '10px';
+    toggleBtn.style.height = '50px';
+    toggleBtn.style.width = '50px';
+    toggleBtn.style.borderRadius = '100%';
+    toggleBtn.style.borderColor = buttonColor;
+    toggleBtn.style.accentColor = buttonColor;
+    toggleBtn.style.backgroundColor = buttonColor;
+    toggleBtn.style.backgroundColor = buttonColor;
+    toggleBtn.style.zIndex = '100';
+    toggleBtn.style.color = 'white';
+    document.body.appendChild(toggleBtn);
+
+})('http://localhost:3000', true, '#007bff');
