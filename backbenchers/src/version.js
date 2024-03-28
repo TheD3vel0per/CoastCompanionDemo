4609afae332380e2597bcd12e4b092bbf7735ee9
matthewkang <matt0410@students.cs.ubc.ca>
2024-03-28 12:41:13 -0700

    0.5.1 database maintenance

--- backbenchers/src/version.js
+++ backbenchers/src/version.js
@@ -1,11 +1,24 @@
 // This file is used to keep track of the latest deployed version of the app
+// And keep status of the current version of the app (active, inactive, etc.)
+
+
 // put the newest deployed version here
 // in "0.x.x" format
 
-const version = "0.5.1"
+export const version = "0.5.1"
+
+// This section is for when the app is not running (down) and not running to 
+// the latest released version. This will be used to display a message on the front end
+
+// Set preset messages for when the app is down here.
+// do not change this message for updating status. Use the availability constant for that.
+const presetAvailabilityMessages = {
+    "db": "The Coast Companion Demo (Chatbot and Admin Panel) is temporarily offline for scheduled database maintenance. Some features may be inaccessible during this time. If you are looking to test or try out the demo, please check again later. We apologize for any inconvenience and appreciate your patience.",
+}
+
+// Set as...
+// export const availabilty = null; // for when the app is running (no message displayed)
+// export const availabilty = presetAvailabilityMessages["db"]; // for when the database is down
+export const availabilty = presetAvailabilityMessages["db"];
 
 
-
-
-
-export default version;
