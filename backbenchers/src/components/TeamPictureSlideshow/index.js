0bf005262424d9badaa840060cbc2e56df7f46e9
matthewkang <matt0410@students.cs.ubc.ca>
2024-04-07 17:20:46 -0700

    Carousel loading fix and added to roadmap page

--- backbenchers/src/components/TeamPictureSlideshow/index.js
+++ backbenchers/src/components/TeamPictureSlideshow/index.js
@@ -5,7 +5,6 @@ import Carousel from 'react-material-ui-carousel'
 import { Paper, Button } from '@mui/material'
 
 export const TeamPictureSlideshow = (props) => {
-  const [firstImgLoaded, setFirstImgLoaded] = React.useState(false);
 
   var items = ["/CoastCompanionDemo/img/team/scream-toad.gif",
     "/CoastCompanionDemo/img/team/scream-toad.gif",
@@ -15,24 +14,15 @@ export const TeamPictureSlideshow = (props) => {
     "/CoastCompanionDemo/img/team/scream-toad.gif",
     "/CoastCompanionDemo/img/team/scream-toad.gif"]
 
-
-
-
   return (
     <>
-      <img
-        src={items[0]}
-        onLoad={() => setFirstImgLoaded(true)}
-        style={{ display: "none" }}
-      />
-      {firstImgLoaded &&
         <Carousel
           animation={"slide"}
           indicators={false}>
           {
             items.map((item, i) => <Item key={i} imgLink={item} imgHeight={props.imgHeight} />)
           }
-        </Carousel>}
+        </Carousel>
     </>
   )
 }
@@ -40,7 +30,7 @@ export const TeamPictureSlideshow = (props) => {
 function Item(props) {
   return (
     <>
-      <img src={props.imgLink} style={{ margin: 0, padding: 0, height: "30%" }} alt='Backbenchers Logo' />
+      <img src={props.imgLink} style={{ margin: 0, padding: 0, width: "100%" }} alt='Backbenchers Logo' />
       <img src='/CoastCompanionDemo/img/backbenchers-logo-small.png' style={{ position: 'absolute', bottom: "5%", right: "2.5%", width: "10%", filter: "drop-shadow(0 0 8px rgba(255, 255, 255, 0.5))", margin: 0, padding: 0 }} alt="Backbenchers Logo" />
     </>
   )
